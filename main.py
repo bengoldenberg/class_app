42682606fa68768d9db849a0f1e46371f9fa43d9
amitde69 <48737176+amitde69@users.noreply.github.com>
2021-07-26 23:06:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Students(BaseModel):
+
+student_list = []
+
+class Student(BaseModel):
     firstname: str
     lastname: str
     id: int
-    mission: list[str] = []
+    mission: list
 
 @app.post("/class/students")
-def create_student(student: Students):
-    return student
+def create_student(student: Student):
+    student_list.append(student)
+    return student_list
 
 @app.put("/class/students")
-def create_student(student: Students):
-    return student
-
+def create_student(new_student: Student):
+    for index, student in enumerate(student_list):
+        if student.id == new_student.id:
+            student_list[index] = new_student
+    return student_list
 
 @app.get("/class/students")
-def get_students(student: Students):
-    return student
+def get_students():
+    return student_list
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=80)
